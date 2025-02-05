5420c0dc608a785abdbc5c1410c70b495548ed8c
hkjin <wlsgusrb78@naver.com>
2025-02-06 00:22:00 +0900

    init

---
+++ src/main/java/org/hyeonqz/jpabestexample/JpaBestExampleApplication.java
@@ -0,0 +1,13 @@
+package org.hyeonqz.jpabestexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBestExampleApplication {
+
+	public static void main (String[] args) {
+		SpringApplication.run(JpaBestExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hyeonqz/jpabestexample/JpaBestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyeonqz.jpabestexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBestExampleApplicationTests {
+
+	@Test
+	void contextLoads () {
+	}
+
+}
